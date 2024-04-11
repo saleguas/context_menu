31817dcd7807a63e1750235e9e3930f83c65d682
Sal <salvadoraleguas@gmail.com>
2024-04-11 02:25:46 -0400

    updated to 1.4.0 on pip and also changed upload script

--- build_pip.bat
+++ build_pip.bat
@@ -2,4 +2,4 @@ del build /Q
 del context_menu.egg-info /Q
 del dist /Q
 python setup.py sdist bdist_wheel
-python -m twine upload dist/*
+python -m twine upload --repository-url https://upload.pypi.org/legacy/ -u __token__ -p your_token_here dist/*
